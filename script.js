1eeeafab68ce6ffaaec7c4d27916cbd3fb857a81
Mariam <mariam.mantidze1@gmail.com>
2024-02-20 16:34:14 +0400

    add event listeners to numbers and operators

--- script.js
+++ script.js
@@ -14,5 +14,34 @@ function checkInputValue(value) {
   }
 }
 
-// body.classList.toggle("light");
-// body.classList.toggle("dark");
+// const executiveBtns = Array.from(document.querySelectorAll(".executive-btn"));
+
+const numbers = Array.from(document.querySelectorAll(".number"));
+const operators = Array.from(document.querySelectorAll(".operators"));
+const deleteBtn = document.getElementById("delete-btn");
+const resetBtn = document.getElementById("reset-btn");
+const equalBtn = document.getElementById("equal-btn");
+const result = document.getElementById("result");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    result.textContent += number.textContent;
+  });
+});
+
+let currentInput = "";
+let previousInput = "";
+let currentOperator = "";
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    previousInput = currentInput; // save the input
+    currentOperator = operator.textContent; // save the operator
+    result.textContent = ""; // clear display
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  currentInput = result.textContent; // store the current display balue
+  result.textContent = calculate(previousInput, currentInput, currentOperator);
+});
